24c4447f19b44bea2fd1feed271251cc73121195
maaz10415 <maaz10415@gmail.com>
2026-04-28 20:58:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+sections.forEach(sec=>{
+observer.observe(sec);
+});
+
+/* SMOOTH SCROLL FOR HEADER LINKS */
+document.querySelectorAll(".nav-links a[href^='#']").forEach(link=>{
+link.addEventListener("click", function(e){
+e.preventDefault();
+
+const target = document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+behavior:"smooth",
+block:"start"
+});
+});
+});
+
+/* HERO BUTTON SCROLL */
+document.getElementById("scrollBtn").addEventListener("click", function(e){
+e.preventDefault();
+document.querySelector("#projects").scrollIntoView({
+behavior:"smooth",
+block:"start"
+});
+});
